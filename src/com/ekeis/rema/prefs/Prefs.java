5c814a0cb181af853739a73bca5a6b02995e6fdc
Elias Keis <elKei24@users.noreply.github.com>
2016-05-29 23:35:35 +0200

    some improvements to prefs

--- src/com/ekeis/rema/prefs/Prefs.java
+++ src/com/ekeis/rema/prefs/Prefs.java
@@ -1,12 +1,17 @@
 package com.ekeis.rema.prefs;
 
 import java.util.ResourceBundle;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 /**
  * @author Elias Keis (29.05.2016)
  */
 public class Prefs {
+    private static final Logger log = Logger.getLogger(Prefs.class.getName());
+
     private static final String KEY_PREFIX = "settings.";
     private static final String KEY_REGISTERS = KEY_PREFIX + "registers";
 
@@ -16,7 +21,22 @@ public class Prefs {
     private Preferences prefs;
 
     private Prefs() {
-        prefs = Preferences.userRoot();
+        prefs = Preferences.userRoot().node("com/ekeis/rema");
+        try {
+            prefs.flush();
+        } catch (BackingStoreException e) {
+            e.printStackTrace();
+            log.warning("Could not flush preferences");
+        }
+    }
+
+    private void sync() {
+        try {
+            prefs.sync();
+        } catch (BackingStoreException e) {
+            e.printStackTrace();
+            log.warning("Could not sync preferences");
+        }
     }
 
     public int getNumberRegisters() {
@@ -24,5 +44,6 @@ public class Prefs {
     }
     public void setNumberRegisters(int val) {
         prefs.putInt(KEY_REGISTERS, val);
+        sync();
     }
 }
